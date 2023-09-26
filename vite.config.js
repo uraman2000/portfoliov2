a641a484898991a904c32f1cb3385ae01e419357
Pol Imbing <uraman2000@gmail.com>
2023-09-27 00:44:35 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    base: "https://www.polimbing.dev/",
-  },
+  base: "https://www.polimbing.dev/",
 })
