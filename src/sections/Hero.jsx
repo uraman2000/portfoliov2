3e1de500a410aec3507402f73346282ce67fd06b
Pol Imbing <uraman2000@gmail.com>
2023-09-27 00:31:57 +0800

    test

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,6 +4,7 @@ import GithubIcon from "../assets/GithubIcon"
 
 import Typewriter from "typewriter-effect"
 export default function Hero({ innerRef }) {
+  console.log(import.meta.env.BASE_URL)
   return (
     <section
       ref={innerRef}
@@ -49,6 +50,7 @@ export default function Hero({ innerRef }) {
             <LinkdnIcon />
           </a>
           <a
+            download
             href={"/pol-imbing-v2.pdf"}
             className="invisible w-0 md:visible md:w-max absolute md:relative md: disabled:bg-gray-600 flex items-center py-2 px-4 bg-blue-600 enabled:hover:bg-blue-700 text-white font-semibold rounded-full shadow-lg transition duration-300 ease-in-out transform enabled:hover:scale-105"
           >
