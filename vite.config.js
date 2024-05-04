66ff8dec48ccddd6c1174cd62b5a560ae881517f
Pol Imbing <uraman2000@gmail.com>
2024-05-05 06:37:20 +0800

    add google analytics

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import VitePluginRadar from "vite-plugin-radar"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: "G-FH10136QC5", // Replace with your Google Analytics ID
+      },
+    }),
+  ],
 })
